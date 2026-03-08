3a007b477d57c298d989f91ef58ff9c4ccb24ee1
GopiAjt <gopiajt23@gmail.com>
2026-03-08 20:51:54 +0530

    feat: implement environment variable support using dotenv

--- capturenow/src/main/java/com/capturenow/CapturenowApplication.java
+++ capturenow/src/main/java/com/capturenow/CapturenowApplication.java
@@ -5,12 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @ComponentScan(basePackages = "com.capturenow")
 
 public class CapturenowApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
         SpringApplication.run(CapturenowApplication.class, args);
     }
